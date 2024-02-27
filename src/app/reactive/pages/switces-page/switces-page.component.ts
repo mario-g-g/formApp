0c03637c73261bdf071fbd9b4d5cbabbb0bd8444
mario-g-g <mario.gil@civica-soft.com>
2024-02-27 11:44:44 +0100

    Seccion 18 final

--- src/app/reactive/pages/switces-page/switces-page.component.ts
+++ src/app/reactive/pages/switces-page/switces-page.component.ts
@@ -1,9 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switces-page.component.html',
   styles: ``
 })
-export class SwitcesPageComponent {
+export class SwitcesPageComponent implements OnInit {
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [ true, Validators.required ],
+    termsAndConditions: [ false, Validators.requiredTrue ],
+  })
+
+  public person = {
+    gender: 'F',
+    wantNotifications: false
+  }
+
+  constructor( private fb: FormBuilder ) {}
+
+  ngOnInit():void {
+    this.myForm.reset ( this.person )
+  }
+
+
+  //ngSubmit()
+  onSave(){
+    if( this.myForm.invalid ){
+      this.myForm.markAllAsTouched()
+      return
+    }
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value
+
+    this.person = newPerson
+    console.log(this.myForm.value);
+    console.log(this.person);
+  }
+
+  isValidField( field:string ): boolean | null{
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched
+  }
+
+  getFieldError( field: string ): string | null {
+    if( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch( key ){
+        case 'required':
+           return 'Este campo es requerido:'
+
+        case 'minlength':
+          return `Mínimo ${ errors['minlength'].requiredLength } caracteres.`
+      }
+    }
+
+    return null
+  }
 
 }
---
+++ src/app/shared/validators/email-validator.service.ts
@@ -0,0 +1,48 @@
+
+
+import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { Observable, delay, of } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class EmailValidator implements AsyncValidator {
+
+  validate(control: AbstractControl<any, any>): Observable<ValidationErrors | null> {
+
+    const email = control.value
+    console.log({ email })
+
+    const httpCallObservable = new Observable<ValidationErrors|null>( ( subscriber ) => {
+
+      console.log({ email })
+
+      if ( email === 'mario@google.com' ){
+        subscriber.next({ emailTaken: true })
+        subscriber.complete()
+      }
+
+      subscriber.next(null)
+      subscriber.complete()
+
+    }).pipe(
+      delay(3000)
+    )
+
+    return httpCallObservable;
+
+  }
+
+  // validate(control: AbstractControl<any, any>): Observable<ValidationErrors | null> {
+
+  //   const email = control.value
+  //   console.log({ email })
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay( 2000 )
+  //   )
+
+  // }
+
+}
